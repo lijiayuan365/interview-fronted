e20723fdfd6e8536e4b1657425e2daa0d9c36dcf
lijiayuan365 <lijiayuan365@gmail.com>
2025-04-13 22:06:04 +0800

    feat: nuxt 子应用注册

---
+++ apps/sub-nuxt/pages/demo/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Demo Page</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ apps/sub-nuxt/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Nuxt Page</h1>
+    <NuxtLink to="/demo">Go to Demo</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+
+</script>
